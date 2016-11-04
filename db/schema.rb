14df2078babceee5dd1eecb20b2098d8d7fd3ba0
Andi <andicbuchanan@gmail.com>
2016-11-04 05:57:13 +0000

    add status column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102040527) do
+ActiveRecord::Schema.define(version: 20161104055640) do
 
   create_table "submissions", force: :cascade do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20161102040527) do
     t.datetime "time"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "status"
   end
 
 end
