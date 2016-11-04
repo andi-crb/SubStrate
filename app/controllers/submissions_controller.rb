6c31db2750c40e8a244d939e0a17beda86e38088
Andi <andicbuchanan@gmail.com>
2016-11-04 06:35:26 +0000

    added filter buttons, kept trying to format form

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -69,6 +69,6 @@ class SubmissionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def submission_params
-      params.require(:submission).permit(:name, :email, :submission, :coverletter, :time)
+      params.require(:submission).permit(:name, :email, :submission, :coverletter, :time, :status)
     end
 end
