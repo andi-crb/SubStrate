41526c4df27a80180d4600632832ed8720e199db
Andi <andicbuchanan@gmail.com>
2016-11-08 07:24:18 +0000

    add user scaffolding

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,3 +1,4 @@
 class Submission < ActiveRecord::Base
+    include Filterable
     scope :status, -> (status) { where status: status }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161104055640) do
+ActiveRecord::Schema.define(version: 20161108052948) do
 
   create_table "submissions", force: :cascade do |t|
     t.string   "name"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20161104055640) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "status"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.integer  "target_days"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
