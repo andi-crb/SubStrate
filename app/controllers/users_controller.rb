67f286f9cbb90ef4c5b60aa3854581c0985dec9b
Andi <andicbuchanan@gmail.com>
2016-11-11 20:02:53 +0000

    sign up working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email, :password_digest, :target_days)
+      params.require(:user).permit(:name, :email, :password, :target_days)
     end
 end
