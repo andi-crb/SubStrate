a951c7d3cab58f6e3c890598bee4993ba2fa8908
Andi <andicbuchanan@gmail.com>
2016-11-14 04:26:37 +0000

    creating rating scaffold

--- db/migrate/20161114042528_create_ratings.rb
+++ db/migrate/20161114042528_create_ratings.rb
@@ -3,6 +3,7 @@ class CreateRatings < ActiveRecord::Migration
     create_table :ratings do |t|
       t.integer :user
       t.integer :submission
+      t.date :date
       t.integer :rating
 
       t.timestamps null: false
